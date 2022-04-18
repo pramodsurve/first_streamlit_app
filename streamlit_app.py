105e992ebe1861701edf4eeacf94538e459cdb85
pramodsurve <59003538+pramodsurve@users.noreply.github.com>
2022-04-18 17:03:23 -0500

    Update streamlit_app.py
    Added filter based on the user input data.

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,9 +23,9 @@ my_fruit_list = pd.read_csv(file_loc)
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Add multi selector picker so thaty users can pick the fruit they want to include
-st.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
-
+fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page
-st.dataframe(my_fruit_list)
+st.dataframe(fruits_to_show)
 
