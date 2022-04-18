1c6841c2761cccfc2b44d6e11f0e34419cb00b63
pramodsurve <59003538+pramodsurve@users.noreply.github.com>
2022-04-18 16:51:08 -0500

    Update streamlit_app.py
    Imported data in pandas and displayed with streamlit.

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,3 +14,12 @@ st.text('🥑 🍞 Avocado Toast')
 
 st.header('🍌 🍓 Build Your Own Fruit Smoothie 🥝 🍇')
 
+
+import pandas as pd
+
+file_loc = 'https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt'
+
+my_fruit_list = pd.read_csv(file_loc)
+
+st.dataframe(my_fruit_list)
+
