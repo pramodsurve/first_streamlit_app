7c5525536a6e973e493eba31b01989e0961bc1c0
pramodsurve <59003538+pramodsurve@users.noreply.github.com>
2022-05-10 09:35:28 -0500

    Added if-else logic

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,23 +41,25 @@ st.dataframe(fruits_to_show)
 # New Section to Display Fruityvice api response
 st.header('Fruityvice Fruit Advice!')
 
-# Get Fruit input
-fruit_choice = st.text_input('What fruit would you like information about?', 'Kiwi')
-st.write('The user entered', fruit_choice)
-
-# import requests
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# take the json version and normalize it
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-
-#Output the normalized version on the screen as a table
-st.dataframe(fruityvice_normalized)
+# If-Then construct
+try:
+  fruit_choice = st.text_input('What fruit would you like information about?')
+  
+  if:
+    not fruit_choice:
+      st.error("Please select a fruit to get information.")
+    else:
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+      # take the json version and normalize it
+      fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+      #Output the normalized version on the screen as a table
+      st.dataframe(fruityvice_normalized)
+      
+except URLError as e:
+  st.error()
 
 # Don't run anything past here until we troubleshoot
 st.stop()
-
 
 # import snowflake.connector
 
